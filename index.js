88461f4873ca918715420ff308ac7a843b89a850
Luiz Viana de Queiroz Neto <luizdequeiroz@outlook.com.br>
2019-08-14 17:35:17 -0300

    adicionando functions object.serializeToUrl e string.toTitleCase e summaries para functions

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 module.exports = () => {
+    /**
+     * Função para agrupar itens de uma lista orientado a um field específico.
+     * @param {string} fieldKey
+     * @param {string} listName
+     */
     Array.prototype.groupBy = function (fieldKey, listName) {
         const groups = this.reduce((groups, item) => {
             const field = item[fieldKey];
@@ -18,6 +23,10 @@ module.exports = () => {
         });
     };
 
+    /**
+     * Função para extrair a diferença, em dias, entre a data atual e outra posterior.
+     * @param {Date} otherDate
+     */
     Date.prototype.subtract = function (otherDate) {
         var oneDay = 1000 * 60 * 60 * 24;
 
@@ -28,4 +37,24 @@ module.exports = () => {
 
         return Math.round((diffMs / oneDay) + 1);
     };
+
+    /**
+     * Função para serializar um objeto em parâmetros de url (key=value).
+     */
+    Object.prototype.serializeToUrl = function () {
+        return `?${Object.keys(this).reduce(
+            (a, k) => {
+                a.push(`${k}=encodeURIComponent(${this[k]})`);
+                return a;
+            }, []).join('&')}`;
+    }
+
+    /**
+     * Função para transformar apenas a primeira letra de cada palavra em caixa alta.
+     */
+    String.prototype.toTitleCase = function () {
+        return this.replace(/\w\S*/g,
+            (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+        );
+    }
 };
